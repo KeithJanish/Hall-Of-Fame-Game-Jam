50e4ba63c83176da844d6be80d8855dd8c5d054e
AlexTheAccount <alexander.james.swanson@gmail.com>
2025-03-09 23:47:19 -0400

    finished Mr. Dream Guy, except for the death sprite

--- Assets/Scripts/Main/Player.cs
+++ Assets/Scripts/Main/Player.cs
@@ -10,18 +10,23 @@ public class Player : MonoBehaviour
     public Rigidbody2D rigbody;
     public GameObject dcFab; // DreamCirclePrefab
     public GameObject dcObj; // DreamCircleObject
+    public SpriteRenderer dcSprRend;
+    public Sprite dcSprA;
+    public Sprite dcSprB;
+    public bool dcSecEnter;
     public Vector2 moveToHere;
     public Vector2 dcObjThrowPoint;
-    public int spawnDelayTimer;
+    public int spriteTimer;
     public float speed;
-    public SpriteRenderer sprRendStand;
-    //public SpriteRenderer sprRendRunLeft;
-    //public SpriteRenderer sprRendRunRight;
-    //public SpriteRenderer sprRendDeath;
+    public SpriteRenderer sprRend;
+    public Animator ani;
+    public bool sPressedWhenDcExist;
+    public AudioSource walk;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigbody = GetComponent <Rigidbody2D>();
+        ani.Play("D0_Nothing");
     }
 
     // Update is called once per frame
@@ -30,64 +35,105 @@ public class Player : MonoBehaviour
         horMove = Input.GetAxis("Horizontal");
 
         rigbody.AddForce(new Vector2(horMove * speed, 0));
-        if (Input.GetKey(KeyCode.A))
-        {
-            sprRendStand.flipX = true;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            sprRendStand.flipX = false;
-        }
 
-        if (rigbody.linearVelocity.x < -1)
+        if (horMove < 0)
         {
-            //sprRendRunLeft.enabled = true;
+            sprRend.flipX = false;
+            ani.Play("A_Player_Run_Left");
+            if (walk.isPlaying == false)
+            {
+                walk.Play();
+            }
         }
-        else if (rigbody.linearVelocity.x > 1)
+        else if (horMove > 0)
         {
-            //sprRendRunLeft.enabled = true;
+            sprRend.flipX = false;
+            ani.Play("A_Player_Run_Right");
+            if (walk.isPlaying == false)
+            {
+                walk.Play();
+            }
         }
         else
         {
-            //sprRendRunLeft.enabled = false;
+            ani.Play("D0_Nothing");
+            walk.Stop();
+            if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x)
+            {
+                sprRend.flipX = true;
+            }
+            else if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x)
+            {
+                sprRend.flipX = false;
+            }
         }
 
         Debug.Log("AAAAA");
         if (Input.GetKeyDown(KeyCode.Mouse0) && dcObj == null)
         {
-            spawnDelayTimer = 100;
+            dcSecEnter = false;
+            sPressedWhenDcExist = false;
+            spriteTimer = 0;
             dcObj = Instantiate(dcFab, transform.position, Quaternion.identity);
             dcObjThrowPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            dcSprRend = dcObj.GetComponent<SpriteRenderer>();
         }
         else
         {
+            spriteTimer += 1;
+            if(spriteTimer >= 7)
+            {
+                if (dcSprRend.sprite == dcSprA)
+                {
+                    dcSprRend.sprite = dcSprB;
+                }
+                else
+                {
+                    dcSprRend.sprite = dcSprA;
+                }
+                spriteTimer = 0;
+            }
+
             if (Input.GetKey(KeyCode.W))
             {
-                moveToHere = (dcObj.transform.position - transform.position) * 2;
+                moveToHere = (dcObj.transform.position - transform.position) * 20;
 
                 rigbody.AddForce(moveToHere);
+            }
+
+            if (sPressedWhenDcExist == false)
+            {
+                dcObj.transform.position = Vector2.MoveTowards(dcObj.transform.position, dcObjThrowPoint, 0.1f);
             }
 
             if (Input.GetKey(KeyCode.S))
             {
                 dcObj.transform.position = Vector2.MoveTowards(dcObj.transform.position, transform.position, 0.1f);
+                sPressedWhenDcExist = true;
             }
-            else
-            {
-                dcObj.transform.position = Vector2.MoveTowards(dcObj.transform.position, dcObjThrowPoint, 0.1f);
-            }
-            spawnDelayTimer -= 1;
         }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Trigger");
-        if (other.CompareTag("DreamCircle") && spawnDelayTimer <= 0)
+        if (other.CompareTag("DreamCircle"))
         {
-            Destroy(other.gameObject);
-            Debug.Log("DreamCircle Destroyed");
+            if (dcSecEnter == true)
+            {
+                Destroy(other.gameObject);
+                Debug.Log("DreamCircle Destroyed");
+            }
         }
     }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Trigger");
+        if (other.CompareTag("DreamCircle"))
+        {
+            dcSecEnter = true;
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision");
